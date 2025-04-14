6065a4538c8f29af82d50b1cc37c0d0726abfe87
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2025-04-14 17:56:52 -0500

    added  all_books, So I can check that the user hasn't read the books being recommended

--- user_info.py
+++ user_info.py
@@ -4,6 +4,7 @@ import re
 class UserInfo():
     def __init__(self):
         self.bookshelf = []
+        self.all_books = []
 
     def story_graph_csv(self,file):
         with open (file,'r',encoding='utf-8',errors='ignore') as f:
@@ -16,6 +17,15 @@ class UserInfo():
                 }
                 for row in reader
                 if row['Star Rating'] and row['ISBN/UID'] and float(row['Star Rating']) >= 4]
+            self.all_books += [
+                {
+                    "Title": row['Title'],
+                    "ISBN": re.search(r'\d+', row['ISBN13']).group(),
+                    "Author": row['Author'],
+                }
+                for row in reader
+                if row['My Rating'] and row['ISBN'] and row['ISBN'] != '=""'
+            ]
             self.sort_shelf()
 
     def good_reads_csv(self,file):
@@ -29,8 +39,19 @@ class UserInfo():
                 }
                 for row in reader
                 if row['My Rating'] and row['ISBN'] and row['ISBN'] !='=""' and float(row['My Rating']) >= 4]
+            self.all_books += [
+                {
+                    "Title": row['Title'],
+                    "ISBN": re.search(r'\d+', row['ISBN13']).group(),
+                    "Author": row['Author'],
+                }
+                for row in reader
+                if row['My Rating'] and row['ISBN'] and row['ISBN'] != '=""'
+            ]
             self.sort_shelf()
 
     def sort_shelf(self):
-        self.bookshelf = [book for id,book in enumerate(self.bookshelf) if book not in self.bookshelf[id+1:]]
+        self.bookshelf = [book for id, book in enumerate(self.bookshelf) if book not in self.bookshelf[id+1:]]
+        self.all_books = [book for id, book in enumerate(self.bookshelf) if book not in self.bookshelf[id+1:]]
+
 
