22bf2c636b103ff8f6d3306c52d8478cfc776177
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2025-04-14 17:56:30 -0500

    added  all_books, So I can check that the user hasn't read the books being recommended

--- book_shelf.py
+++ book_shelf.py
@@ -1,16 +1,26 @@
 import requests
 class BookShelf:
-    def __init__(self):
-        pass
+    def __init__(self,books):
+        self.books = books
+        for book in books:
+           self.book_info(book['ISBN'])
 
     def book_info(self,isbn):
         url=f"https://openlibrary.org/api/volumes/brief/isbn/{isbn}.json"
-        print(url)
         search = requests.get(url)
         book = search.json()
-        book_id = next(iter(book['records']), None)
-        book_info = book['records'][book_id]
-        return book_info
+        if book != []:
+            print(url)
+            book_id = next(iter(book['records']), None)
+            book_info = book['records'][book_id]['data']
+            print(book_info['title'])
+            items = book['items']
+            if len(items) > 0:
+                print(items)
+            return book_info
+        return "Not found in Open Library"
+
+
 
 
 
