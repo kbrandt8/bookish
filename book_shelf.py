74d449e080caa29b35407f95a62076eaccaf6b6a
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2025-05-01 23:00:27 -0500

    More already read book filters, there's more information being taken in the book_info, the filtering is more flexible

--- book_shelf.py
+++ book_shelf.py
@@ -10,6 +10,9 @@ class BookShelf:
         self.recommendations = []
         self.recommendations_isbn = []
         self.edition_keys = []
+        self.keys = []
+        self.owned_books = []
+        self.book_info({'Title':'Dracula','Author':'Bram Stoker'})
 
     def get_recs(self):
         for book in self.books:
@@ -19,34 +22,36 @@ class BookShelf:
             self.search_subject(subject)
         self.set_recs()
         file = "recommendations.csv"
-        fields = ['Title','Subjects']
+        fields = ['Title','Subjects','Key']
         with open (file,'w',encoding='utf-8') as rec_file:
             csvwriter = csv.writer(rec_file)
             csvwriter.writerow(fields)
             for book in self.recommendations:
-                csvwriter.writerow([book['title'],book['subject']])
+                csvwriter.writerow([book['title'],book['key'],book['subject']])
 
     def book_info(self, book):
-        url = f"https://openlibrary.org/search.json?title={book['Title']}&author={book['Author']}&fields=edition_key,title,subject,isbn,author"
+        url = f"https://openlibrary.org/search.json?title={book['Title']}&author={book['Author']}&fields=edition_key,title,subject,isbn,author_name,key"
         search = requests.get(url)
         result = search.json()
         for search_book in result['docs']:
-            if search_book['title'] == book['Title']:
+            if book['Title'] in search_book['title']  and book['Author'] in search_book['author_name']:
+                self.owned_books.append(book['Title'])
                 self.subjects += search_book.get("subject", [])
                 self.isbn_list += search_book.get("isbn", [])
                 self.edition_keys += search_book.get("edition_key",[])
+                self.keys.append(search_book.get("key",[]))
 
 
     def set_subjects(self):
         to_delete = ["Romans, nouvelles", "Fiction", "Large type books", "General", "Children's fiction",
-                     "Reading Level-Grade 11", "Reading Level-Grade 12"]
+                     "Novela"]
         subject_tally = dict((i, self.subjects.count(i)) for i in self.subjects)
         all_subjects = sorted(subject_tally.items(), key=lambda item: item[1], reverse=True)
         new_subjects = [subject for subject, tally in all_subjects if subject not in to_delete and tally > 3]
         self.subjects = new_subjects
 
     def search_subject(self, subject):
-        url = f"https://openlibrary.org/search.json?subject={subject}&fields=title,subject,isbn,key"
+        url = f"https://openlibrary.org/search.json?subject={subject}&fields=title,subject,isbn,key,edition_key,author_name"
         search = requests.get(url)
         if 'application/json' in search.headers.get('Content-Type', ''):
             search_json = search.json()
@@ -56,14 +61,16 @@ class BookShelf:
                     all_subjects = [subject for subject in book['subject']]
                     shared_subjects = [subject for subject in all_subjects if subject in self.subjects]
                     if len(shared_subjects) > 4 and 'isbn' in book:
-                        edited_book = {'title': book['title'],'isbn':book['isbn'],'subject':[subject for subject in book['subject'] if subject in self.subjects]}
+                        edited_book = {'title': book['title'],'isbn':book['isbn'],'key':book['key'],'edition_key':book['edition_key'],'subject':[subject for subject in book['subject'] if subject in self.subjects]}
                         self.recommendations.append(edited_book)
 
     def set_recs(self):
         new_recommendations = []
         for key, book in enumerate(self.recommendations):
-           isbn = any(element in book['isbn'] for element in self.isbn_list)
-           if not isbn:
-               if book not in new_recommendations:
+            isbn = any(element in book['isbn'] for element in self.isbn_list)
+            edition_key = any(element in book['edition_key'] for element in self.edition_keys)
+            key = True if book['key'] in self.keys else False
+            title = True if book['title'] in self.owned_books else False
+            if not isbn and not key and not edition_key and not title and book not in new_recommendations:
                     new_recommendations.append(book)
         self.recommendations = sorted(new_recommendations, reverse=True, key=lambda d: len(d['subject']))
