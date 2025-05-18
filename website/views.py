6b479e15650bc9a67c3a2c6e56807d93d30f2827
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2025-05-17 19:34:09 -0500

    flask stubs are setup

--- website/views.py
+++ website/views.py
@@ -1,6 +1,23 @@
 from flask import Blueprint, render_template
-
+import json
 views = Blueprint('views', __name__)
+
+
 @views.route("/")
 def home():
     return render_template("index.html")
+@views.route("/search/<search_type>")
+def search(search_type):
+    return render_template("search.html",type=search_type)
+@views.route("/results")
+def results():
+    # Just a placeholder for now
+    with open('recommendations.json','r') as f:
+        data = json.load(f)
+    return render_template("results.html",result=data['books'])
+@views.route("/account")
+def account():
+    return render_template("account.html")
+@views.route("/watchlist")
+def watchlist():
+    return render_template("watchlist.html")
