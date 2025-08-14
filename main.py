78cd26dab56282f9bc539bbcb7aba666b0b28ffb
Kaitlyn Brandt <kaitlyn.brandt@proton.me>
2025-08-13 23:46:45 -0500

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from website import create_app,db
-
+from website import create_app, db
+from flask import current_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, threaded=True)
+# Optional: create tables automatically on import
+with app.app_context():
+    db.create_all()
